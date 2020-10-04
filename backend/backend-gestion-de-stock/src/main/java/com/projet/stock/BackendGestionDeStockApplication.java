084e0f8e3cdb6abc335437f85e202d62c1318afd
Houssem ben jemia <houssembenjemia@gmail.com>
2020-10-04 17:32:29 +0100

    initialisation backend spring boot

---
+++ backend/backend-gestion-de-stock/src/main/java/com/projet/stock/BackendGestionDeStockApplication.java
@@ -0,0 +1,13 @@
+package com.projet.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGestionDeStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGestionDeStockApplication.class, args);
+	}
+
+}
